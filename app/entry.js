16b38421439e116dd972aeba422d511583a14075
Sean Lee <freshdried@gmail.com>
2015-03-14 00:11:12 -0400

    updated boilerplate

--- app/entry.js
+++ app/entry.js
@@ -1,10 +1,4 @@
-//import React from "react";
-//import App from "./App.jsx";
+import React from "react";
+import App from "./App.jsx";
 
-import style from "./folder/style.scss";
-import poop from "poop/poop.scss";
-import test from "test";
-console.log(test);
-document.write(test);
-
-//React.render(<App />, document.getElementById("root"));
+React.render(<App />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,12 @@
 
 var webpack = require("webpack");
 
-var MY_MODULES = "/home/slee2/webpack";
+var palette_dir = process.env.PALETTE_DIR;
+if (palette_dir) {
+    console.log("Palette directory recognized at " + palette_dir);
+} else {
+    console.log("Error! Palette directory not found!");
+}
 
 module.exports = {
     devtools: "eval",
@@ -24,10 +29,14 @@ module.exports = {
     resolve: {
         extensions: ["", ".js", ".jsx"],
         root : __dirname,
+        /*
+         * We need to alias React because of how react-hot-loader
+         * transforms modules to require react and react/lib/ReactMount
+         */
         alias: {
-            react: MY_MODULES + "/node_modules/react"
+            react: __dirname + "/node_modules/react"
         },
-        fallback: MY_MODULES
+        fallback: palette_dir
 
     },
 
@@ -35,6 +44,7 @@ module.exports = {
         { test: /\.scss$/, loaders: ["react-hot", "style", "css", "sass"]},
         { test: /\.css$/, loader: ["react-hot", "style", "css"]},
         { test: /\.md$/, loader:  ["react-hot", "html", "markdown"]},
+        { test: /\.html$/, loader:  ["react-hot", "html"]},
         { test: /\.json$/, loader: ["react-hot", "json"]},
         { test: /\.jsx?$/, loaders: ["react-hot", "babel"], exclude: /node_modules/}
     ]},
