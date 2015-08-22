ce65ff0f101dfbb4db3424449d1e3097daa29b96
Sean Lee <freshdried@gmail.com>
2015-08-22 15:02:17 -0400

    added sourcemaps

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const App = React.createClass({
     render() {
+        console.log("App rendered!");
         return <div id="App">
             <h1>Hello World</h1>
             <div>The DOM is your oyster!</div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,84 +1,60 @@
-"use strict";
 var webpack = require("webpack");
 
-var IS_PRODUCTION = process.env.NODE_ENV === "production";
-var PRODUCTION_HOSTNAME = process.env.HOSTNAME || "localhost";
+var isprod = process.env.NODE_ENV === "production";
+console.log(isprod ? "Production Mode" : "Development Mode");
 
-console.log( IS_PRODUCTION ? "Production Mode" : "Development Mode");
 
-var config = {};
+var entry = isprod ? [ 
+    "./app/entry.jsx"
+] : [ 
+    "webpack-dev-server/client?http://localhost:8080",
+    "webpack/hot/only-dev-server",
+    "./app/entry.jsx"
+];
 
-config.output = {
-    path: __dirname + "/dist/",
-    filename: "bundle.js",
-    publicPath: IS_PRODUCTION ? "http://localhost:8080/" : "http://localhost:8080/"
-}
 
-config.resolve = {
-    extensions: ["", ".js", ".jsx"],
-    root : __dirname,
-    /*
-     * We need to alias React because of how react-hot-loader
-     * transforms modules to require react and react/lib/ReactMount
-     */
-    // alias: {
-    //     react: __dirname + "/node_modules/react"
-    // },
-}
+var plugins = isprod ? [
+    new webpack.optimize.UglifyJsPlugin({
+        compressor: {
+            warnings: false
+        }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.NoErrorsPlugin()
+] : [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+];
 
-config.module = { loaders: [
-    { test: /\.scss$/, loaders: ["style", "css", "sass"]},
-    { test: /\.css$/, loaders: ["style", "css" ]},
-    { test: /\.md$/, loaders:  ["html", "remarkable"]},
-    { test: /\.html$/, loaders:  ["html"]},
-    { test: /\.json$/, loaders: ["json"]},
-    { test: /\.jsx?$/, loaders: IS_PRODUCTION ? ["babel?stage=0"] : ["react-hot", "babel?stage=0"] , exclude: /node_modules/},
-    { test: /\.(png|jpg|jpeg|gif|svg)$/, loaders: ["url?limit=10000"]},
-    { test: /\.(woff|woff2)$/, loaders: ["url?limit=100000"]},
-    { test: /\.(ttf|eot)$/, loaders: ["file"]},
-    { test: /\.(mp3|wav)$/, loaders: ["file"]}
-]};
 
-config.remarkable = {
-    breaks: true,
-    html: true
+module.exports = {
+    output: {
+        path: __dirname + "/dist/",
+        filename: "bundle.js",
+        publicPath: "http://localhost:8080/"
+    },
+
+    resolve: {
+        extensions: ["", ".js", ".jsx"]
+    },
+
+    module: { loaders: [
+        { test: /\.scss$/, loaders: ["style", "css", "sass"]},
+        { test: /\.css$/, loaders: ["style", "css" ]},
+        { test: /\.md$/, loaders:  ["html", "remarkable"]},
+        { test: /\.html$/, loaders:  ["html"]},
+        { test: /\.json$/, loaders: ["json"]},
+        { test: /\.jsx?$/, loaders: isprod ? ["babel?stage=0"] : ["react-hot", "babel?stage=0"] , exclude: /node_modules/},
+        { test: /\.(png|jpg|jpeg|gif|svg)$/, loaders: ["url?limit=10000"]},
+        { test: /\.(woff|woff2)$/, loaders: ["url?limit=100000"]},
+        { test: /\.(ttf|eot)$/, loaders: ["file"]},
+        { test: /\.(mp3|wav)$/, loaders: ["file"]}
+    ]},
+    remarkable : {
+        breaks: true,
+        html: true
+    },
+    entry: entry,
+    plugins: plugins,
+    devtool: isprod ? "source-map" : "cheap-module-eval-source-map"
 };
-
-
-config.entry = IS_PRODUCTION
-    ?
-    [ 
-        "webpack-dev-server/client?http://" + PRODUCTION_HOSTNAME + ":8080",
-        "./app/entry.jsx"
-    ]
-    : 
-    [ 
-        "webpack-dev-server/client?http://localhost:8080",
-        "webpack/hot/only-dev-server",
-        "./app/entry.jsx"
-    ];
-
-
-
-
-config.plugins = IS_PRODUCTION
-    ?
-    [
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {
-                warnings: false
-            }
-        }),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.NoErrorsPlugin()
-    ]
-    : 
-    [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
-    ];
-
-config.devtools = IS_PRODUCTION ? undefined : "source-map"
-
-
-module.exports = config;
