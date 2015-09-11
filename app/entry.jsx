5ff99b6a9b00a15db2ad7fb9c353d4e267840725
Sean Lee <freshdried@gmail.com>
2015-09-11 18:02:07 -0400

    updated to react-router 1.0.0beta4

--- app/entry.jsx
+++ app/entry.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import "./style.scss"
 
 import {Router, Route, Link} from "react-router";
-import HashHistory from 'react-router/lib/HashHistory'
+import createHistory from "history/lib/createHashHistory";
 
 
 import App from "./App.jsx";
@@ -10,7 +11,7 @@ import Landing from "./Landing.jsx";
 
 
 React.render((
-    <Router history={new HashHistory()}>
+    <Router history={createHistory({queryKey: false})}>
         <Route path="/" component={App}
                indexRoute={{component: Landing}}>
         </Route>
