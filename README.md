56274c95a446d831536426078f23427505f5a02f
Sean Lee <freshdried@gmail.com>
2015-09-11 16:58:59 -0400

    updated to react-transform

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-# react-hot-seed
-webpack project seed with [react-hot-loader](https://github.com/gaearon/react-hot-loader/)
+# react-seed
+webpack project seed with [react-transform](http://github.com/gaearon/babel-plugin-react-transform)
 
 Switchable production/development modes via `$NODE_ENV`
 
 ## features
+- hot reloading with [react-transform-webpack-hmr](https://github.com/gaearon/react-transform-webpack-hmr)
+- error catching with [react-transform-catch-errors](https://github.com/gaearon/react-transform-catch-errors)
 - support for jsx, es6, via [babel](https://babeljs.io/)
-- loader for markdown via [remarkable-loader](https://github.com/unindented/remarkable-loader)
+- support for markdown via [remarkable-loader](https://github.com/unindented/remarkable-loader)
 - loaders for sass, css, html, json, images, audio, fonts
-- defaults to development mode (hot-loading); switchable to production mode (minification) via `$NODE_ENV`
-- react-router v1.0
+- defaults to development mode (hot-reloading); switchable to production mode (minification) via `$NODE_ENV`
+- [react-router v1.0](https://github.com/rackt/react-router/tree/master)
--- server.js
+++ server.js
@@ -1,18 +1,41 @@
-var webpack = require("webpack");
-var WebpackDevServer = require("webpack-dev-server");
-var config = require("./webpack.config");
+"use strict";
 
-var server = new WebpackDevServer(webpack(config), {
-    contentBase: __dirname + "/dist/",
-    publicPath: config.output.publicPath,
-    hot: (process.env.NODE_ENV !== "production"),
-    historyApiFallback: true
+const path = require("path");
+const express = require("express");
+const webpack = require("webpack");
+const config = require("./webpack.config");
+
+const dev  = require("webpack-dev-middleware");
+const hot = require("webpack-hot-middleware");
+
+
+
+let app = express();
+
+let compiler = webpack(config);
+
+app.use(dev(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.use(hot(compiler));
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "dist/index.html"));
 });
 
-server.listen(8080, function (err, result) {
-    if (err) {
-        console.log(err);
-    }
-    console.log("Listening at port 8080");
+app.listen(8080, "localhost", function (err) {
+  if(err) {
+    console.log(err);
+    return;
+  }
 });
 
+
+let TODELTE = {
+  contentBase: __dirname + "/dist/",
+  publicPath: config.output.publicPath,
+  hot: (process.env.NODE_ENV !== "production"),
+  historyApiFallback: true
+};
