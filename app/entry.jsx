b16cafb2bd7e879e8b7c30763d0b4f5b85f1b2b9
Sean Lee <freshdried@gmail.com>
2015-09-12 11:35:07 -0400

    moved to plain routes

--- app/entry.jsx
+++ app/entry.jsx
@@ -8,11 +8,16 @@ import createHistory from "history/lib/createHashHistory";
 import App from "./App.jsx";
 import Landing from "./Landing.jsx";
 
+let routes = {
+    path: "/",
+    component: App,
+    indexRoute: {component: Landing},
+    childRoutes: [
+    ]
+}
+
 
 React.render((
-    <Router history={createHistory({queryKey: false})}>
-        <Route path="/" component={App}
-               indexRoute={{component: Landing}}>
-        </Route>
-    </Router>
+    <Router history={createHistory({queryKey: false})}
+            children={routes}/>
 ), document.getElementById("app"));
