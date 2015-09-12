83f0ef651cc1c8ee729da13a9eea3a01c8973bd1
Sean Lee <freshdried@gmail.com>
2015-09-12 10:46:25 -0400

    reverted back to react 0.13

--- app/entry.jsx
+++ app/entry.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./style.scss"
 
 import {Router, Route, Link} from "react-router";
@@ -10,7 +9,7 @@ import App from "./App.jsx";
 import Landing from "./Landing.jsx";
 
 
-ReactDOM.render((
+React.render((
     <Router history={createHistory({queryKey: false})}>
         <Route path="/" component={App}
                indexRoute={{component: Landing}}>
